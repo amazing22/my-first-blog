3f668f15e4e980f77c2f0bdda16eca7e5f6e4faf
Your Name <you@example.com>
2017-09-15 00:41:52 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
 
+
 # ^ : 문자열이 시작할 때
 # $ : 문자열이 끝날 때
 # \d : 숫자
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
