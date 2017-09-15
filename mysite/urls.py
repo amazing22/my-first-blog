cdc088cbf3a3f48b82b0dc1fe75435e0f6bca38f
amazing22 <djagpwls20@gmail.com>
2017-09-16 01:15:33 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
-<link rel = "shortcuticon" herf="/favicon.ico"/>
+<link rel = "icon" href="/favicon.ico" type="image/x-icon"/>
+<link rel = "shortcut icon" href ="/favicon.ico" type="image/x-icon"/>
 
 # 파이썬에서는 정규 표현식을 작성할 때 항상 문자열 앞에 r을 붙인다.
 # 파이썬에게 문자열에 특수 문자가 있다는 것을 알려줌.
