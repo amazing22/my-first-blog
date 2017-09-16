da7c7948714ed032d0f9241d253b65d17ef82682
Your Name <you@example.com>
2017-09-17 00:40:31 +0900

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views # 추가
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'), # 추가
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}), # 추가
     url(r'', include('blog.urls')),
 ]
 
