0129c2917f59167fee68e5150cf8b606bdb5ab82
Your Name <you@example.com>
2017-09-17 11:48:05 +0900

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey('blog.Post', related_name='comment')
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
