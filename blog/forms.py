52f692074506486f24f8f772c3a07b5ab0430c87
Your Name <you@example.com>
2017-10-03 22:35:13 +0900

    코멘트전으로 돌리기

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post
+# , Comment
 
 class PostForm(forms.ModelForm):
 
@@ -7,8 +8,8 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ('author', 'text',)
+# class CommentForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Comment
+#         fields = ('author', 'text',)
